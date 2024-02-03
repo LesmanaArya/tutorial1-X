7d69176cbcf441ad5078c3e3579ebbc2b667dd8b
Arya Lesmana <arya.lesmana21@ui.ac.id>
2024-02-03 22:56:32 +0700

    Sudah selesai hingga Run Application

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Iterator;
 
+@Repository
 public class ProductRepository {
     private List<Product> productData = new ArrayList<>();
 
