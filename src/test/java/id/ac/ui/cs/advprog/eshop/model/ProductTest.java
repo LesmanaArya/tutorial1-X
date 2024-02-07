89319af38a476068823b11434d65213b028ac14d
Arya Lesmana <arya.lesmana21@ui.ac.id>
2024-02-07 14:56:00 +0700

    Done Unit Test

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/ProductTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/ProductTest.java
@@ -4,7 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-public class ProductTest {
+
+class ProductTest {
     Product product;
     @BeforeEach
     void setup() {
